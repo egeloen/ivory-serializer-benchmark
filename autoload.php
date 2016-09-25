b9b2f48abfe4809a079b8a621c3cf9cfc4ef98ab
GeLo <geloen.eric@gmail.com>
2016-09-26 00:24:08 +0200

    Remove apc class loader + update deps

--- autoload.php
+++ autoload.php
@@ -1,13 +1,8 @@
 <?php
 
 use Doctrine\Common\Annotations\AnnotationRegistry;
-use Symfony\Component\ClassLoader\ApcClassLoader;
 
 $loader = require __DIR__.'/vendor/autoload.php';
 AnnotationRegistry::registerLoader([$loader, 'loadClass']);
 
-$apcLoader = new ApcClassLoader(sha1(__FILE__), $loader);
-$loader->unregister();
-$apcLoader->register(true);
-
-return $apcLoader;
+return $loader;
